8e037a9cd10f62faf4d990ebb690182779a0da69
Maulik Dave <mauldave@gmail.com>
2025-06-06 19:54:49 +0530

    add logback.xml with level info, add multiple consumers in gbif-connector and make GBIFClient multi-threaded

--- impact-gbif-connector/src/main/java/in/foresthut/impact/App.java
+++ impact-gbif-connector/src/main/java/in/foresthut/impact/App.java
@@ -32,78 +32,104 @@ public class App {
 	private final static Config config = Config.getInstance();
 	private final static String TASK_QUEUE_NAME = config.get("rabbitmq.csis-tasks.queue.name");
 	private final static String OBSERVATIONS_QUEUE_NAME = config.get("rabbitmq.observations.queue.name");
+	private final static int NUMBER_OF_CONSUMERS = 4;
 
 	public static void main(String[] args)
 			throws IOException, InterruptedException, ExecutionException, TimeoutException {
 		processedMessageCollection = csisProcessedMessagesDB.getCollection("messageIds");
 
+		GBIFClient gbifClient = GBIFClient.getInstance();
+
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost(config.get("rabbitmq.csis-tasks.host"));
 		Connection connection = factory.newConnection();
+
+		// Creating 4 consumers
+		for (int i = 0; i < NUMBER_OF_CONSUMERS; i++) {
+			createConsumers(connection, i + 1, gbifClient);
+		}
+	}
+
+	private static void createConsumers(Connection connection, int consumerId, GBIFClient gbifClient)
+			throws IOException {
+		// Create a channel for each consumer
 		Channel channel = connection.createChannel();
+		channel.basicQos(1);
 
+		// Declare the queue (if it doesn't exist)
 		channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
-		logger.info(" [*] Waiting for messages. To exit press CTRL+C");
 
+		// Create a consumer
 		boolean autoAck = false;
-		channel.basicConsume(TASK_QUEUE_NAME, autoAck, "myConsumerTag", new DefaultConsumer(channel) {
-			@Override
-			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
-					byte[] body) throws IOException {
-				long deliveryTag = envelope.getDeliveryTag();
-				Task message = new ObjectMapper().readValue(body, Task.class);
-				logger.info(" [x] Received '" + message + "'");
+		channel.basicConsume(TASK_QUEUE_NAME, autoAck, String.valueOf("Consumer" + consumerId),
+				new DefaultConsumer(channel) {
+					@Override
+					public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+							byte[] body) throws IOException {
+						long deliveryTag = envelope.getDeliveryTag();
+						Task message = new ObjectMapper().readValue(body, Task.class);
+						logger.info("Consumer {} received {}", consumerId, message);
 
-				// Check for a duplicate message in consumer Db
-				int hash = message.messageHash();
-				long count = processedMessageCollection
-						.countDocuments(new BsonDocument("messageId", new BsonInt32(hash)));
+						// Check for a duplicate message in consumer Db
+						int hash = message.messageHash();
+						long count = processedMessageCollection
+								.countDocuments(new BsonDocument("messageId", new BsonInt32(hash)));
 
-				if (count == 0) {
-					try {
-						List<Observation> observations = new GBIFClient().observations(message);
-						logger.info("Received {} observations from GBIF for ecoregionId {} and hash {}",
-								observations.size(), message.ecoregionId(), message.messageHash());
-						// 1. Put observations in Observations QUEUE
-						for (var observation : observations) {
+						if (count == 0) {
 							try {
-								publishObservation(observation);
-							} catch (IOException | TimeoutException e) {
-								logger.error("Error while publishing {} to observations queue", observation, e);
-								throw new IOException(e);
+								List<Observation> observations = gbifClient.observations(message);
+								logger.info("Received {} observations from GBIF for ecoregionId {} and hash {}",
+										observations.size(), message.ecoregionId(), message.messageHash());
+								// 1. Put observations in Observations QUEUE
+								for (var observation : observations) {
+									try {
+										//Add ecoregionId to message and publish
+										ObservationMessage observationMessage = new ObservationMessage(message.ecoregionId(), observation);
+										publishObservation(observationMessage);
+									} catch (IOException | TimeoutException e) {
+										logger.error("Error while publishing {} to observations queue", observation, e);
+										throw new IOException(e);
+									}
+								}
+								// 2. Add messageHash to consumer db
+								processedMessageCollection.insertOne(new Document("messageId", hash));
+								// 3. Ack task queue to confirm message processing
+								channel.basicAck(deliveryTag, false);
+
+							} catch (InterruptedException | ExecutionException e) {
+								channel.basicNack(deliveryTag, false, true);
+								logger.error("Error while received the message from queue {}", TASK_QUEUE_NAME, e);
 							}
+						} else {
+							logger.info("Duplicate message for hash {}", hash);
+							channel.basicAck(deliveryTag, false);
 						}
-						// 2. Add messageHash to consumer db
-						processedMessageCollection.insertOne(new Document("messageId", hash));
-						// 3. Ack task queue to confirm message processing
-						channel.basicAck(deliveryTag, false);
-
-					} catch (InterruptedException | ExecutionException e) {
-						channel.basicNack(deliveryTag, false, true);
-						logger.error("Error while received the message from queue {}", TASK_QUEUE_NAME, e);
 					}
-				} else {
-					logger.info("Duplicate message for hash {}", hash);
-					channel.basicAck(deliveryTag, false);
-				}
-			}
 
-			private void publishObservation(Observation observation) throws IOException, TimeoutException {
-				ConnectionFactory factory = new ConnectionFactory();
-				factory.setHost(config.get("rabbitmq.observations.host"));
-				try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
-					channel.queueDeclare(OBSERVATIONS_QUEUE_NAME, false, false, false, null);
-					var message = new ObjectMapper().writeValueAsBytes(observation);
-					channel.basicPublish("", OBSERVATIONS_QUEUE_NAME, null, message);
-					logger.info(" [x] Sent for GBIF observation id '{}'", observation.key());
-				} catch (IOException e) {
-					logger.error("{}", e);
-					throw e;
-				} catch (TimeoutException e1) {
-					logger.error("{}", e1);
-					throw e1;
-				}
-			}
-		});
+					private void publishObservation(ObservationMessage observation) throws IOException, TimeoutException {
+						ConnectionFactory factory = new ConnectionFactory();
+						factory.setHost(config.get("rabbitmq.observations.host"));
+						try (Connection connection = factory.newConnection();
+								Channel channel = connection.createChannel()) {
+							channel.queueDeclare(OBSERVATIONS_QUEUE_NAME, false, false, false, null);
+							var message = new ObjectMapper().writeValueAsBytes(observation);
+							channel.basicPublish("", OBSERVATIONS_QUEUE_NAME, null, message);
+							// logger.info(" [x] Sent for GBIF observation id '{}'", observation.key());
+						} catch (IOException e) {
+							logger.error("{}", e);
+							throw e;
+						} catch (TimeoutException e1) {
+							logger.error("{}", e1);
+							throw e1;
+						}
+					}
+				});
+
+		logger.info("Consumer " + consumerId + " is waiting for messages.");
+	}
+	
+	static record ObservationMessage(String ecoregionId, Observation observation) {
 	}
 }
+
+
--- impact-gbif-connector/src/main/java/in/foresthut/impact/gbif/GBIFClient.java
+++ impact-gbif-connector/src/main/java/in/foresthut/impact/gbif/GBIFClient.java
@@ -8,7 +8,11 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,35 +35,62 @@ public class GBIFClient {
 	private final HttpClient client;
 	private final ObjectMapper objectMapper;
 
-	public GBIFClient() {
+	private static GBIFClient instance;
+
+	public synchronized static GBIFClient getInstance() {
+		if (instance == null)
+			instance = new GBIFClient();
+		return instance;
+	}
+
+	private GBIFClient() {
 		client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build();
 		objectMapper = new ObjectMapper();
 	}
 
+	// TODO - send a page retrieval request in separate threads once number of pages
+	// are known
 	public List<Observation> observations(Task task) throws IOException, InterruptedException, ExecutionException {
 		List<Observation> observations = new ArrayList<>();
-		boolean endOfRecords = false;
 		int offset = 0;
-		while (!endOfRecords) {
-			final String GBIF_URL = GBIF_HOST + "/occurrence/search?limit=" + LIMIT + "&offset=" + offset + "&modified="
-					+ URLEncoder.encode(task.dateFrom(), "UTF-8") + "," + URLEncoder.encode(task.dateTo(), "UTF-8")
-					+ "&geometry=" + URLEncoder.encode(task.polygon(), "UTF-8");
+		String gbifUrl = GBIF_HOST + "/occurrence/search?occurrenceStatus=PRESENT&limit=" + LIMIT + "&offset=" + offset
+				+ "&modified=" + URLEncoder.encode(task.dateFrom(), "UTF-8") + ","
+				+ URLEncoder.encode(task.dateTo(), "UTF-8") + "&geometry=" + URLEncoder.encode(task.polygon(), "UTF-8");
 
-			try {
-				HttpRequest request = HttpRequest.newBuilder().uri(URI.create(GBIF_URL))
-						.header("User-Agent", "sayhello@foresthut.in").GET().build();
-				HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		try {
+			HttpRequest request = HttpRequest.newBuilder().uri(URI.create(gbifUrl))
+					.header("User-Agent", "sayhello@foresthut.in").GET().build();
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			if (response.statusCode() == 200) {
 				GBIFResponse gbifResponse = objectMapper.readValue(response.body(), GBIFResponse.class);
+				logger.info("Total {} observations to be fetched.", gbifResponse.count());
 				observations.addAll(gbifResponse.results());
-				endOfRecords = gbifResponse.endOfRecords();
-				offset += LIMIT;
 				logger.info("Received {} observations for page number {}", gbifResponse.results().size(),
-						offset / LIMIT);
-			} catch (Exception e) {
-				logger.error("Error while getting response for URL {}", GBIF_URL, e);
-				throw e;
+						(offset + LIMIT) / LIMIT);
+
+				if (!gbifResponse.endOfRecords()) {
+					for (int i = offset + LIMIT; i < gbifResponse.count(); i += LIMIT) {
+						gbifUrl = GBIF_HOST + "/occurrence/search?occurrenceStatus=PRESENT&limit=" + LIMIT + "&offset="
+								+ i + "&modified=" + URLEncoder.encode(task.dateFrom(), "UTF-8") + ","
+								+ URLEncoder.encode(task.dateTo(), "UTF-8") + "&geometry="
+								+ URLEncoder.encode(task.polygon(), "UTF-8");
+						try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+							Future<List<Observation>> future = executor
+									.submit(new FetchGBIFObservationsTask(client, gbifUrl, task, i));
+							observations.addAll(future.get());
+						}
+					}
+				}
+
+			} else {
+				logger.error("HTTP error {} for task {}", response.statusCode(), task);
+				throw new IOException(String.format("Http error %d for task %s", response.statusCode(), task));
 			}
+		} catch (Exception e) {
+			logger.error("Error while getting response for URL {}", gbifUrl, e);
+			throw e;
 		}
+
 		logger.info("Received {} observations for task {}", observations.size(), task);
 		return observations;
 	}
@@ -73,6 +104,46 @@ public class GBIFClient {
 	public static record Observation(long key, String species, String kingdom, String phylum, String order,
 			@JsonProperty(value = "class") String _class, String iucnRedListCategory, double decimalLatitude,
 			double decimalLongitude, String eventDate, String modified, String datasetName) {
+	}
+
+	static class FetchGBIFObservationsTask implements Callable<List<Observation>> {
+		private HttpClient client;
+		private String url;
+		private Task task;
+		private int offset;
+		private final ObjectMapper objectMapper;
+
+		public FetchGBIFObservationsTask(HttpClient client, String url, Task task, int offset) {
+			this.client = client;
+			this.url = url;
+			this.task = task;
+			this.offset = offset;
+			this.objectMapper = new ObjectMapper();
+		}
+
+		@Override
+		public List<Observation> call() throws Exception {
+			List<Observation> observations = new ArrayList<>();
+			try {
+				HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url))
+						.header("User-Agent", "sayhello@foresthut.in").GET().build();
+				HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+				if (response.statusCode() == 200) {
+					GBIFResponse gbifResponse = objectMapper.readValue(response.body(), GBIFResponse.class);
+					observations = gbifResponse.results();
+					logger.info("Received {} observations for page number {}", observations.size(),
+							(offset / LIMIT) + 1);
+				} else {
+					logger.error("HTTP error {} for task {}", response.statusCode(), task);
+					throw new IOException(String.format("Http error %d for task %s", response.statusCode(), task));
+				}
+			} catch (Exception e) {
+				logger.error("Error while getting response for URL {}", url, e);
+				throw e;
+			}
+			return observations;
+		}
+
 	}
 
 }
